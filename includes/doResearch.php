bc1d277005238dd7fe78644fde670a71dbce5df3
Guillaume GT. Tejedor <guillaume@ajpi.local>
2021-04-28 13:19:47 +0200

    ajout modif

--- includes/doResearch.php
+++ includes/doResearch.php
@@ -9,32 +9,33 @@
 		//Initialize variables
 		$pattern = $_POST["patternInput"];
 		$nbRecord = 0;
-		$query = "select * from sales where " .  $elementType . " like '%" . $pattern . "%' ";
+		$clause = $elementType . " like '%" . $pattern . "%' ";
 
 		//Verify if user has added start date
 		if(isset($_POST['startPeriod']) && !empty($_POST['startPeriod'])){
 			$startPeriod = $_POST['startPeriod'];
-			$query .= "and date >= '" . $startPeriod . "' ";
+			$clause .= "and date >= '" . $startPeriod . "' ";
 		}
 
 		//Verify if user has added end date
 		if(isset($_POST['endPeriod']) && !empty($_POST['endPeriod'])){
 			$endPeriod = $_POST['endPeriod'];
-			$query .= "and date <= '" . $endPeriod . "' ";
+			$clause .= "and date <= '" . $endPeriod . "' ";
 		}
 
 		//Query to fetch data matching with the searched pattern
+		$query = "select * from sales where " . $clause;
 		$record = $connection->query($query)->fetchAll();
 		$nbRecord = $connection->query($query)->fetchColumn();
-
 	}
 	//Otherwise, we display the most recently added data
 	else
 	{
-		$query = "select * from sales order by date desc";
-
 		//Query to fetch recent data
+		$query = "select * from sales order by date desc";
 		$record = $connection->query($query)->fetchAll();
+
+		$query = "select count(*) from sales";
 		$nbRecord = $connection->query($query)->fetchColumn();
 	}
 ?>
