6ffc69770c17200501d6b7ae5a32539703886b9a
Guillestre <guillaume.tejedor@outlook.fr>
2021-05-10 12:30:15 +0200

    ajout modif

--- index.php
+++ index.php
@@ -3,7 +3,29 @@
 
 	<head>
 		<?php include 'ext/header.php';?>
-		<?php include 'invoiceLineFixSage2016.php' ?>
+		<?php 
+			/*
+			include 'sage2016_clients.php';
+			include 'sage2016_invoices.php';
+			include 'sage2016_invoiceLine.php';
+
+			include 'sage2019_clients.php';
+			include 'sage2019_invoices.php';
+			include 'sage2019_invoiceLine.php';
+
+			include 'sage_clients.php';
+			include 'sage_invoices.php';
+			include 'sage_invoiceLine.php';
+
+			include 'odoo_clients.php';
+			include 'odoo_invoices.php';
+			include 'odoo_invoiceLine.php';
+
+			include 'clients.php';
+			include 'invoices.php';
+			include 'invoiceLine.php';
+			*/
+		?>
 		<title>Accueil</title>
 	</head>
 
@@ -11,7 +33,7 @@
 
 		<!-- CONNECTION TO DATABASE ---->
 		<?php 
-			//header("Location: dashboard.php");
+			header("Location: dashboard.php");
 		?>
 
 	</body>
